c1405f88745bf66f5759e73e161a747e47de9149
Jessica <58232904+JessicaPalhano@users.noreply.github.com>
2022-07-08 13:33:30 -0300

    adicionar, excluir, editar e listar produtos

---
+++ src/server.js
@@ -0,0 +1,42 @@
+const porta = 3003
+
+const express = require('express')
+const app = express()
+const bodyParse = require('body-parser')
+const dataBase = require('./database')
+
+app.use(bodyParse.urlencoded({ extended: true }))
+
+app.get('/produtos', (req, res, next) => {
+  res.send(dataBase.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+  res.send(dataBase.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+  const produto = dataBase.salvarProduto({
+    nome: req.body.nome,
+    preco: req.body.preco
+  })
+  res.send(produto)
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+  const produto = dataBase.salvarProduto({
+    id: req.params.id,
+    nome: req.body.nome,
+    preco: req.body.preco
+  })
+  res.send(produto)
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+  const produto = dataBase.excluirProduto(req.params.id)
+  res.send(produto)
+})
+
+app.listen(porta, () => {
+  console.log(`Servidor executando na Porta ${porta}.`)
+})
